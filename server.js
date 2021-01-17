ce6d4a557cb16574ac543a33efa270df2d3a4cad
nimrod vain <togithub1@gmail.com>
2021-01-17 17:19:54 +0200

    init commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path')
 
 const app = express();
 
@@ -14,6 +15,13 @@ app.use('/profile', profile);
 app.use('/buzzings', buzzings);
 
 connectDB();
+
+if(process.env.NODE_ENV == "production") {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
